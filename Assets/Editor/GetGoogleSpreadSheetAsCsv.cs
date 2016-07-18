90ea23be78bcab23cf7220640fc577e161f0a76f
Mitsutaka Takeda <mitsutaka.takeda@gmail.com>
2016-07-19 01:25:15 +0900

    Try to get a google spread sheet as a csv file

---
+++ Assets/Editor/GetGoogleSpreadSheetAsCsv.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetGoogleSpreadSheetAsCsv : UnityEditor.EditorWindow {
+
+    const string sheetURL = "https://docs.google.com/feeds/download/spreadsheets/Export?key=1D52aCQzAyjM4Cttw2ecv_dC5SXRzRMZ92MdbFWqJsaY&exportFormat=csv&gid=0";
+    string message;
+    string sheetName;
+
+    [UnityEditor.MenuItem("Window/Get GoogleSpreadSheet As CSV")]
+    static void Open()
+    {
+        GetWindow<GetGoogleSpreadSheetAsCsv>();
+    }
+
+    void OnGUI()
+    {
+        UnityEditor.EditorGUILayout.Space();
+        sheetName = UnityEditor.EditorGUILayout.TextField(
+            "sheet name",
+            sheetName,
+            GUILayout.Height(20.0f));
+        UnityEditor.EditorGUILayout.Space();
+
+        if (GUILayout.Button("Create", GUILayout.Width(50.0f)))
+        {
+            var download = new WWW(sheetURL);
+            while (!download.isDone) ;
+            Debug.Log(download.text);
+        }
+
+        UnityEditor.EditorGUILayout.Space();
+        //UnityEditor.EditorGUILayout.LabelField(message);
+    }
+}
